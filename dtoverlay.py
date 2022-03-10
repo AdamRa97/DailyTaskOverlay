ba1ea8d30931632a6649ad2243b8ce576e3dfdcd
Sehoon <adamra97@gmail.com>
2022-03-10 15:54:07 -0500

    Added some sound effects

--- dtoverlay.py
+++ dtoverlay.py
@@ -1,6 +1,6 @@
-from cProfile import label
 from tkinter import *
 from tkinter import ttk
+from playsound import playsound
 
 root = Tk()
 root.overrideredirect(True)
@@ -56,27 +56,33 @@ def deleteItem():
 
 def tranSlideVal(val):
     root.attributes("-alpha", int(val)*.1)
-    
-    
-title_bar = Frame(root, bg="grey10", relief="raised", bd=1, cursor="@aero_arrow.cur")
-title_bar.pack(expand=1, fill=X)
-title_label = Label(title_bar, text="Quest Helper", font="Arial", bg="grey10", fg="light goldenrod")
-title_label.pack(side=LEFT)
-title_bar.bind("<B1-Motion>", move_window)
-title_bar.bind("<Button-1>", get_pos)
 
-close_button = Button(title_bar, text="X", command=root.destroy, cursor="@aero_link.ani", bg="grey10", fg="white")
-close_button.pack(side=RIGHT)
+def clickPlay():
+    playsound("click.mp3", False)
 
-current_value = DoubleVar()
+def hoverPlay(event):
+    playsound("hover.mp3", False)
+    
+titleBar = Frame(root, bg="grey10", relief="raised", bd=1, cursor="@aero_arrow.cur")
+titleBar.pack(expand=1, fill=X)
+titleLabel = Label(titleBar, text="Quest Helper", font="Arial", bg="grey10", fg="light goldenrod")
+titleLabel.pack(side=LEFT)
+titleBar.bind("<B1-Motion>", move_window)
+titleBar.bind("<Button-1>", get_pos)
+
+closeButton = Button(titleBar, text="X", command=root.destroy, cursor="@aero_link.ani", bg="grey10", fg="white")
+closeButton.bind("<Enter>", hoverPlay)
+closeButton.pack(side=RIGHT)
+
+currentValue = DoubleVar()
 transLabel = Label(root, text="Transparency", cursor="@aero_arrow.cur")
 transLabel.pack(anchor="w", side="bottom")
-tranSlider = Scale(root, from_=1, to= 10, orient="horizontal", cursor="@aero_arrow.cur", variable=current_value, command=tranSlideVal)
+tranSlider = Scale(root, from_=1, to= 10, orient="horizontal", cursor="@aero_arrow.cur", variable=currentValue, command=tranSlideVal)
 tranSlider.set(10)
 tranSlider.pack(anchor="w", side="bottom")
 
-scroll_bar= Scrollbar(root, cursor="@aero_arrow.cur")
-scroll_bar.pack(side=RIGHT,fill=Y)
+scrollBar= Scrollbar(root, cursor="@aero_arrow.cur")
+scrollBar.pack(side=RIGHT,fill=Y)
 
 tabControl = ttk.Notebook(root, cursor="@aero_arrow.cur")
 tab1 = Frame(tabControl)
@@ -86,18 +92,21 @@ tabControl.pack(expand=1, fill="both")
 tab2 = Frame(tabControl)
 tabControl.add(tab2, text="Una Task To-Do")
 
-createButton = Button(tab1, text="Create", command=createList, cursor="@aero_link.ani")
+createButton = Button(tab1, text="Create", command=lambda:[clickPlay(),createList()], cursor="@aero_link.ani")
+createButton.bind("<Enter>",hoverPlay)
 createButton.pack(anchor="s", side="bottom")
 
-completeButton = Button(tab2, text="Complete", command=deleteItem, cursor="@aero_link.ani")
+completeButton = Button(tab2, text="Complete", command=lambda:[clickPlay(),deleteItem()], cursor="@aero_link.ani")
+completeButton.bind("<Enter>",hoverPlay)
 completeButton.pack(side=BOTTOM)
 
-homeButton = Button(tab2, text="Home", command= selectListTasks, cursor="@aero_link.ani")
+homeButton = Button(tab2, text="Home", command= lambda:[clickPlay(),selectListTasks()], cursor="@aero_link.ani")
+homeButton.bind("<Enter>",hoverPlay)
 homeButton.pack(side=BOTTOM)
 
 taskList = ["nacho", "sandwich", "burger", "pizza", "tacos", "Madness Piles on the Altar", "Fairy's Friend", "Bleak Night Fog"]
 taskList.sort()
-listbox = Listbox(tab1, yscrollcommand = scroll_bar.set, selectmode="multiple", height= 30, width=50, bg = "white", activestyle="dotbox", font="Arial", fg = "black", cursor="@aero_arrow.cur")
+listbox = Listbox(tab1, yscrollcommand = scrollBar.set, selectmode="multiple", height= 30, width=50, bg = "white", activestyle="dotbox", font="Arial", fg = "black", cursor="@aero_arrow.cur")
 for item in range(len(taskList)):
     listbox.insert(END, taskList[item])
     listbox.itemconfig(item, bg="#bdc1d6")
